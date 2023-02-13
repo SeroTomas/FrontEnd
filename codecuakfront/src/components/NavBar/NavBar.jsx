74a0e7421688f92cbf9bb83993c0e11f7ed00b64
unknown <serocontacto@gmail.com>
2023-02-13 17:52:43 -0300

    estructura nav

---
+++ codecuakfront/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <nav className={style.navContainer}>
+        <div className={style.logoContainer}>
+            
+
+        </div>
+        <div className={style.searchContainer}>
+
+        </div>
+        <div className={style.ulContainer}>
+
+
+        </div>
+        <div className={style.iconsContainer}>
+
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar

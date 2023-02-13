0a9e58ac7eca9212c49738f3a75e921f9d49308f
JulianMRivero <riverojulian076@gmail.com>
2023-02-13 17:41:18 -0300

    Sections landing

--- codecuakfront/src/components/Landing/Landing.jsx
+++ codecuakfront/src/components/Landing/Landing.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <>
+      <section>
+        <h1>codeCuak</h1>
+        <h2>La red social por y para desarrolladores</h2>
+        <p>
+          Nace con la idea de crear un espacio amigable en el que los
+          desarrolladores puedan distenderse,
+          <br />
+          compartir ideas y proyectos propios, resolver dudas, buscar compañeros
+          para trabajar en equipo y buscar trabajo.
+          <br /> 
+          Todo dentro de un mismo espacio.
+        </p>
+      </section>
+      <section>
+        <h1>socialCuak</h1>
+        <h2>Aquí podrás expresarte</h2>
+        <p>
+          lo que buscamos con “social-Cuak” es distendernos y conocernos; 
+          <br />
+          encontrar un clima agradable para despejar la mente.
+        </p>
+      </section>
+      <section>
+        <h1>Q&A-Cuak</h1>
+        <h2>Para crecer necesitamos ayudarnos entre todos</h2>
+        <p>
+          Por eso en “Q&A-Cuak” los usuarios podrán generar nuevas publicaciones{" "}
+          <br />
+          solicitando ayuda o responder las consultas ya existentes.
+        </p>
+      </section>
+      <section>
+        <h1>workCuak</h1>
+        <h2>La programación es un trabajo de equipo</h2>
+        <p>“work-Cuack” está diseñado para buscar colaboradores para desarrollar proyectos propios. </p>
+      </section>
+      <section>
+        <h1>hiringCuak</h1>
+        <h2>En codeCuak nos preocupamos por el futuro de nuestros miembros</h2>
+        <p>Ideamos una sección en la que podrán cargar su perfil en el caso de que estén en búsqueda activa de empleo.
+          <br/>
+          Tambien las empresas podrán registrarse y publicar ofertas laborales para que los desarrolladores apliquen a ellas.
+        </p>
+        
+      </section>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;

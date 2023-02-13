0cb421d0178592ab9e5ea5dbe9b9745ca8475d31
JulianMRivero <riverojulian076@gmail.com>
2023-02-13 17:56:22 -0300

    fixed sections

--- codecuakfront/src/components/Landing/Landing.jsx
+++ codecuakfront/src/components/Landing/Landing.jsx
@@ -8,45 +8,44 @@ const Landing = () => {
         <h2>La red social por y para desarrolladores</h2>
         <p>
           Nace con la idea de crear un espacio amigable en el que los
-          desarrolladores puedan distenderse,
-          <br />
-          compartir ideas y proyectos propios, resolver dudas, buscar compañeros
-          para trabajar en equipo y buscar trabajo.
-          <br /> 
-          Todo dentro de un mismo espacio.
+          desarrolladores puedan distenderse, compartir ideas y proyectos
+          propios, resolver dudas, buscar compañeros para trabajar en equipo y
+          buscar trabajo. Todo dentro de un mismo espacio.
         </p>
       </section>
       <section>
-        <h1>socialCuak</h1>
-        <h2>Aquí podrás expresarte</h2>
+        <h2>socialCuak</h2>
+        <h3>Aquí podrás expresarte</h3>
         <p>
-          lo que buscamos con “social-Cuak” es distendernos y conocernos; 
-          <br />
+          lo que buscamos con “social-Cuak” es distendernos y conocernos;
           encontrar un clima agradable para despejar la mente.
         </p>
       </section>
       <section>
-        <h1>Q&A-Cuak</h1>
-        <h2>Para crecer necesitamos ayudarnos entre todos</h2>
+        <h2>Q&A-Cuak</h2>
+        <h3>Para crecer necesitamos ayudarnos entre todos</h3>
         <p>
-          Por eso en “Q&A-Cuak” los usuarios podrán generar nuevas publicaciones{" "}
-          <br />
+          Por eso en “Q&A-Cuak” los usuarios podrán generar nuevas publicaciones
           solicitando ayuda o responder las consultas ya existentes.
         </p>
       </section>
       <section>
-        <h1>workCuak</h1>
-        <h2>La programación es un trabajo de equipo</h2>
-        <p>“work-Cuack” está diseñado para buscar colaboradores para desarrollar proyectos propios. </p>
+        <h2>workCuak</h2>
+        <h3>La programación es un trabajo de equipo</h3>
+        <p>
+          “work-Cuack” está diseñado para buscar colaboradores para desarrollar
+          proyectos propios.
+        </p>
       </section>
       <section>
-        <h1>hiringCuak</h1>
-        <h2>En codeCuak nos preocupamos por el futuro de nuestros miembros</h2>
-        <p>Ideamos una sección en la que podrán cargar su perfil en el caso de que estén en búsqueda activa de empleo.
-          <br/>
-          Tambien las empresas podrán registrarse y publicar ofertas laborales para que los desarrolladores apliquen a ellas.
+        <h2>hiringCuak</h2>
+        <h3>En codeCuak nos preocupamos por el futuro de nuestros miembros</h3>
+        <p>
+          Ideamos una sección en la que podrán cargar su perfil en el caso de
+          que estén en búsqueda activa de empleo. Tambien las empresas podrán
+          registrarse y publicar ofertas laborales para que los desarrolladores
+          apliquen a ellas.
         </p>
-        
       </section>
     </>
   );

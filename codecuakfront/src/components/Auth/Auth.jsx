f79bf5f107536d983a444b6686cbdf09732070a2
Nacho_Devs <100791052+nachitoxx6262@users.noreply.github.com>
2023-02-13 19:08:30 -0300

    add buttons login

--- codecuakfront/src/components/Auth/Auth.jsx
+++ codecuakfront/src/components/Auth/Auth.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import LoginButton from '../blueprints/LoginButton'
-
+import LogoutButton from '../blueprints/LogOut'
+import Profile from '../blueprints/Perfil'
 const Auth = () => {
   return (
     <div>
+      <LoginButton />
+      <LogoutButton/>
+      <Profile />
     </div>
   )
 }

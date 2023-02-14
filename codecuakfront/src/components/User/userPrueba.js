f706a81f1d977f98d3746e5c35d2b17d98e383e2
AugustoCorbalan <auguscorbi@gmail.com>
2023-02-13 22:00:09 -0300

    cree componente Card para user, y objeto de prueba

---
+++ codecuakfront/src/components/User/userPrueba.js
@@ -0,0 +1,7 @@
+export const userPrueba={
+    name: 'Nombre Usuario',
+    email: 'usuario@gmail.com',
+    nickName: 'User',
+    birthdate: '1998/08/25' ,
+    image: 'https://definicion.de/wp-content/uploads/2019/07/perfil-de-usuario.png'
+}

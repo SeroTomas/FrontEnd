459f6b372deb1d4917aad28cf01d115b711e06a9
AugustoCorbalan <auguscorbi@gmail.com>
2023-02-15 20:30:59 -0300

    agregue boton para ocultar comentarios del Post

--- codecuakfront/src/components/User/Cards/Post/CardsComents.jsx
+++ codecuakfront/src/components/User/Cards/Post/CardsComents.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import CardComent from "./CardComent.jsx";
+import { useState } from "react";
 import styles from "./Styles/CardsComents.module.css";
 
 const Coments=({props})=>{
     const coments = props;
+    const [viewComents, setViewComents] = useState(false)
     return(
         <div className={styles.container}>
-            {coments.map((coment)=><CardComent props={coment}/>)}
+            <button className={styles.buttonComents} onClick={()=>setViewComents(!viewComents)}>Comentarios</button>
+            
+            {viewComents? coments.map((coment)=><CardComent props={coment}/>) :
+            <></>
+            }
         </div>
     )
 }

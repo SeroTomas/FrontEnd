b83d38c1a92705e428b899c3292dfa901fb28de5
AugustoCorbalan <auguscorbi@gmail.com>
2023-02-16 19:08:36 -0300

    Posts en ruta de social y algo de estilo en Social

--- codecuakfront/src/components/Social/Post/CardsPost.jsx
+++ codecuakfront/src/components/Social/Post/CardsPost.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useEffect } from "react";
-import CardPost from "./CardPost";
-import { userPrueba } from "../../userPrueba";
-import { postsHardCode } from "../../postSocial";
+import CardPost from "../../User/Cards/Post/CardPost.jsx";
+import { postsHardCode } from "../../User/postSocial";
 import styles from "./Styles/CardsPost.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { getPost } from "../../../../redux/slices/socialPost/socialActions";
+import { getPost } from "../../../redux/slices/socialPost/socialActions";
 
 const CardsPost = () => {
   const data = useSelector((state) => state.socialPost.posts);

b33e76f6038f08b27a1591ae841d9d062b6fe967
Nacho_Devs <100791052+nachitoxx6262@users.noreply.github.com>
2023-02-17 00:19:24 -0300

    o

--- codecuakfront/src/components/User/userPrueba.js
+++ codecuakfront/src/components/User/userPrueba.js
@@ -1,9 +1,9 @@
 export const userPrueba={
-    name: 'Nombre Usuario',
-    email: 'usuario@gmail.com',
-    nickName: 'nickName',
+    name: 'Juan',
+    email: 'juan@gmail.com',
+    nickName: 'Juanpito',
     birthdate: '1998/08/25' ,
-    image: 'https://definicion.de/wp-content/uploads/2019/07/perfil-de-usuario.png' ,
+    image: 'https://www.imagenesbonitasname.com/covers/preview/pues-si-wey-sticker-wasap.jpg' ,
     portada: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTZZeLZAzPyAtBhCUl384gDJYN3ROMfPXtPDUlu3QAX9gyEEX6',
     technicalSkills: ["JavaScript", "React", "Redux", "Nextjs", "Sequelize"],
     softSkills: ["Liderazgo", "Proactividad", "Trabajo en equipo", "Resiliencia", "Empatía"],

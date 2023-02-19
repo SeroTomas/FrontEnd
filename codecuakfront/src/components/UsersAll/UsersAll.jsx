fd31062f2877b3c64b6c1aa5c562a2cf20a211ad
Nacho_Devs <100791052+nachitoxx6262@users.noreply.github.com>
2023-02-19 20:06:31 -0300

    fix

--- codecuakfront/src/components/UsersAll/UsersAll.jsx
+++ codecuakfront/src/components/UsersAll/UsersAll.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { getAllUsers } from "../../redux/action";
 import { useSelector, useDispatch } from "react-redux";
 import NavBar from "../NavBar/NavBar";
 import Users from "./Users";
 import { getPage } from "../../redux/action";import { getUsersAlpha } from "./../../redux/action";
-
+import Style from "./UsersAll.module.css"
 const UsersAll = () => {
   const dispatch = useDispatch();
+  const [pages, setPages] = useState("1")
   const data = useSelector((state) => state.users);
   const pageCount = data.pages;
   const buttons = [];
   const handleClick=(e)=>{
     let page = e.target.value
+    setPages(e.target.value)
     dispatch(getPage(page))
   }
   for (let i = 1; i <= pageCount; i++) {
     buttons.push(
-      <button key={i} value={i} onClick={handleClick}>
+      <button key={i} className={pages==i? Style.button : Style.buttonNone} value={i} onClick={handleClick}>
         {i}
       </button>
     );
@@ -25,6 +27,7 @@ const UsersAll = () => {
   useEffect(() => {
     dispatch(getAllUsers());
   }, [dispatch]);
+
   const handleChange = (event) => {
     event.preventDefault();
     event.target.name == "asc" ? dispatch(getUsersAlpha("asc")) : [];
@@ -38,10 +41,10 @@ const UsersAll = () => {
         return <Users name={user.name} image={user.image} />;
       })}
       {buttons}
-      <button name="asc" onClick={handleChange}>
+      <button name="asc" className={Style.asc} onClick={handleChange}>
         ASC
       </button>
-      <button name="desc" onClick={handleChange}>
+      <button name="desc" className={Style.asc} onClick={handleChange}>
         DESC
       </button>
     </div>

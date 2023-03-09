6ae4179602c7b6c3cf9d3900706d61528795bd6c
JulianMRivero <riverojulian076@gmail.com>
2023-03-08 22:32:55 -0300

    dialog edit

--- codecuakfront/src/components/blueprints/FormDialog/FormDialog.jsx
+++ codecuakfront/src/components/blueprints/FormDialog/FormDialog.jsx
@@ -1,44 +1,45 @@
-import {useState} from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { Box } from '@mui/system';
-import { editPost } from '../../../axiosFunctions';
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Box } from "@mui/system";
+import { editPost } from "../../../axiosFunctions";
 
-export default function FormDialog({setOption, postId, content}) {
+export default function FormDialog({ setOption, postId, content }) {
   const [open, setOpen] = useState(true);
 
-  const [post, setPost] = useState(content)
+  const [post, setPost] = useState(content);
 
   const token = localStorage.getItem("token");
 
   const handleClose = () => {
     setOpen(false);
-    setOption("")
+    setOption("");
   };
 
   const handleChange = (event) => {
     const value = event.target.value;
     setPost(value);
-  }
+  };
 
   const handleEdit = () => {
-    editPost(post, postId, token)
-    setOpen(false)
-  }
-
+    editPost(post, postId, token);
+    setOpen(false);
+  };
 
   return (
-    <Box >
+    <Box>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Editar</DialogTitle>
-        <DialogContent>
+        <DialogContent sx={{ width: "35rem", height: "30rem" }}>
           <TextField
             autoFocus
+            multiline
+            color="success"
             margin="dense"
             id="name"
             label="Que quieres editar?"
@@ -50,8 +51,12 @@ export default function FormDialog({setOption, postId, content}) {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleEdit}>Confirmar edicion</Button>
+          <Button onClick={handleClose} color="success">
+            Cancelar
+          </Button>
+          <Button onClick={handleEdit} color="success">
+            Confirmar edicion
+          </Button>
         </DialogActions>
       </Dialog>
     </Box>
